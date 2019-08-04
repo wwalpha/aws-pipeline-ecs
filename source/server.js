15d176206da08b0b9ee4282d3f63aaf73202ad85
wwalpha <wwalpha@gmail.com>
2019-08-04 12:40:14 +0900

    remove file

--- source/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-app.use(express.static('public'))
-
-const port = process.env.PORT || 80;
-
-app.listen(port, () => console.log('Example app listening on port %s!', port));
